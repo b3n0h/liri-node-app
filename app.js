3a71564b54c5d2dfff5ad4faa1e47e8be0eb6076
benohhh <b3njaminoh@gmail.com>
2018-10-04 16:42:34 -0700

    required spotify

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require("dotenv").config()
+const keys = require('./keys')
 const fs = require('fs')
 const request = require('request')
 const moment = require('moment')
-// const spotify = Spotify(keys.spotify)
+const Spotify = require('node-spotify-api')
+
+
+var spotify = new Spotify ({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+})
 
 if (process.argv[2] == 'concert-this') {
   let artist = process.argv.slice(3).join('%20')
@@ -16,5 +23,16 @@ if (process.argv[2] == 'concert-this') {
       console.log('Venue location: ' + result[1].venue.city)
       console.log('Concert date: ' + moment(result[1].datetime).format('MM/DD/YYYY'))
     }
+  })
+} else if (process.argv[2] == 'spotify-this-song') {
+  spotify.search({
+    type: 'track',
+    query: 'All The Small Things'
+  })
+  .then (function(r) {
+    console.log(r)
+  })
+  .catch(function(e) {
+    console.log(e)
   })
 }
